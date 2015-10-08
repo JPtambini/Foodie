3e7e9b261cf901299086ee9cd7bd8455b28d4207
John Paul Tambini <Tambini.jp@gmail.com>
2015-10-08 17:10:36 -0400

    start of feed.erb

--- main.rb
+++ main.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
-
  get '/' do 
- 	erb:layout
+ 	erb :layout
  end
+ get '/edit' do
+ 	erb :edit
+ end
+ get '/feed' do
+ 	erb :feed
+ end
+ get '/post' do
+ 	erb :post
+ end
+ get 'signup' do
+ 	erb :signup
+ end
+
