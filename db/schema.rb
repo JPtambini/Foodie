95fbc48ce10ca3f0c960b5daf76431915c79a4de
John Paul Tambini <Tambini.jp@gmail.com>
2015-10-09 14:15:34 -0400

    added 2 migrates 1 for post.erb and other for signup.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008215644) do
+ActiveRecord::Schema.define(version: 20151009170550) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "date_time"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
--- main.rb
+++ main.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
+enable :sessions
 
 set :database, "sqlite3:foodie_base.sqlite3"
+
+def add_user(params)
+
+	User.create(fname:params[:first_name], lname:params[:last_name],uname:params[:username],password: params[:password],email:params[:email],location:params[:location])
+end
+
+
+
+
+
+
+
  get '/' do 
  	erb :layout
  end
@@ -19,4 +32,6 @@ set :database, "sqlite3:foodie_base.sqlite3"
  	erb :signup
  end
 
-enable :sessions
+ post "/sign_up" do
+ 	add_user(params)
+end
