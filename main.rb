1867588018300c50ade962a659eedb4e0c173aaa
Victor Tsay <vt0826@hotmail.com>
2015-10-09 17:46:15 -0400

    session

--- main.rb
+++ main.rb
@@ -6,15 +6,14 @@ enable :sessions
 set :database, "sqlite3:foodie_base.sqlite3"
 
 def add_user(params)
-
 	User.create(fname:params[:first_name], lname:params[:last_name],uname:params[:username],password: params[:password],email:params[:email],location:params[:location])
 end
 
-
-
-
-
-
+def current_user
+    if session[:user_id]
+        @current_user=User.find(session[:user_id])
+    end
+end
 
  get '/' do 
  	erb :layout
@@ -22,7 +21,7 @@ end
  get '/edit' do
  	erb :edit
  end
- get '/feed' do
+ get '/mini_feed' do
  	erb :feed
  end
  get '/post' do
@@ -32,6 +31,24 @@ end
  	erb :signup
  end
 
- post "/sign_up" do
+
+post "/sign_up" do
  	add_user(params)
+    
+end
+
+post "/log_out" do
+    session[:username]=nil
+    redirect"/signup"
+end
+
+post "/log_in" do
+    @user=User.where(uname: params[:username]).first
+    if @user.password == params[:password]
+        session[:username] = @user.uname 
+        puts current_user
+        redirect "/mini_feed"
+    else 
+        redirect "/signup"
+    end
 end
